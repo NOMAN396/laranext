7491241ecf7f1870a66b40e14db46452c7abaa58
noman <nomanhossain12276@gmail.com>
2024-01-11 00:54:03 +0600

    nextjswithlara

---
+++ app/mamu/page.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import axios from '@/app/stock/axious'
+import { useEffect } from 'react'
+
+
+export default function Ami() {
+
+
+  useEffect(() => {
+    
+    fetchStock()
+  })
+
+  function fetchStock() {
+    
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+       <h1 className='bg-red'>Student Details</h1>
+      <div>
+       
+        
+
+<form className="max-w-sm mx-auto">
+  <div className="mb-5">
+    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="enter your name" required/>
+  </div>
+  <div className="mb-5">
+    <label htmlFor="father_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Father Name</label>
+    <input type="father_name" id="father_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='enter your father name' required/>
+  </div>
+  <div className="mb-5">
+    <label htmlFor="mobile" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mobile:</label>
+    <input type="mobile" id="mobile" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='enter your mobile' required/>
+  </div>
+  <div className="flex items-start mb-5">
+    <div className="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required/>
+    </div>
+    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+  </div>
+  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
+
+      </div>
+    </main>
+  )
+}
